a679f24f7f42ad3f401ff24c13e00fdc1bcfdca9
YuyuQianRice <Yuyu.Qian.Rice@gmail.com>
2020-11-05 22:20:36 -0600

    Initial Commit

---
+++ UUTwit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UUTwit
+//
+//  Created by Yuyu Qian on 11/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
